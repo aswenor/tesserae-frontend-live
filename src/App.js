29265111ce28a6ab8e230b8038806bda6415e9da
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-01-22 11:14:23 -0500

    set up packages and react-router logic

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
+import NavBar from './global/NavBar';
+
+const Index = () => <h2>Home</h2>;
+const Match = () => <h2>Match</h2>;
+const About = () => <h2>About</h2>;
+
 class App extends Component {
   render() {
+    const routes = [
+      {link: "/", name: "Home"},
+      {link: "/match", name: "Match"},
+      {link: "/about", name: "About"}
+    ]
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <NavBar routes={routes} />
+          <Route path="/" exact component={Index} />
+          <Route path="/match" component={Match} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
     );
   }
 }
