559313c44a8bad1ade6682685d0bbfb0aa94aac6
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-01-22 11:18:37 -0500

    add global nav bar with react-router routes

---
+++ src/global/NavBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const routes = this.props.routes.map((route, i) => {
+      return (
+        <Button component={Link} to={route.link} key={i}>
+          {route.name}
+        </Button>
+      );
+    });
+
+    return (
+      <AppBar className="tess-navbar" position="static">
+        <Toolbar className="">
+          <Typography variant="h6" color="inherit">Tesserae</Typography>
+          {routes}
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+
+NavBar.propTypes = {
+  routes: PropTypes.arrayOf(
+    PropTypes.shape({
+      link: PropTypes.string,
+      name: PropTypes.string
+    }))
+};
+
+export default NavBar;
