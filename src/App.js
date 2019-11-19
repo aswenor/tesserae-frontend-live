6661480ef175f561de83c34de58e3f973f1dd74c
Jeff Kinnison <jeffdkinnison@gmail.com>
2019-11-18 21:01:10 -0500

    set up the app for showing off prototypes

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import './App.css';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
-import NavBar from './global/NavBar';
+import NavBar from './components/common/NavBar';
+import SearchPrototypeA from './components/search/SearchPrototypeA';
 
 const Index = () => <h2>Home</h2>;
-const Match = () => <h2>Match</h2>;
-const About = () => <h2>About</h2>;
 
 class App extends Component {
   render() {
     const routes = [
       {link: "/", name: "Home"},
-      {link: "/match", name: "Match"},
-      {link: "/about", name: "About"}
+      {link: "/searcha", name: "SearchPrototypeA"},
+      //{link: "/about", name: "SearchB"}
     ]
 
     return (
@@ -23,8 +22,7 @@ class App extends Component {
         <div className="App">
           <NavBar routes={routes} />
           <Route path="/" exact component={Index} />
-          <Route path="/match" component={Match} />
-          <Route path="/about" component={About} />
+          <Route path="/searcha" component={SearchPrototypeA} />
         </div>
       </Router>
     );
