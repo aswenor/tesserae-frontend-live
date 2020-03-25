0de80f8cf836674103786f3e833bc63c7f275b12
Jeff Kinnison <jeffdkinnison@gmail.com>
2020-03-25 10:21:15 -0500

    add configurable environment

--- src/api/corpus/index.js
+++ src/api/corpus/index.js
@@ -2,15 +2,16 @@ import axios from 'axios';
 
 import * as actions from '../../state_management/search';
 
+const REST_API = process.env.REACT_APP_REST_API_URL;
+
 
 export function fetchLanguagesAction(pending) {
   return dispatch => {
     if (!pending) {
-    console.log('fetching languages')
       dispatch(actions.fetchLanguagesPending);
       axios({
         method: 'get',
-        url: 'http://45.55.219.221:5000/languages',
+        url: `${REST_API}/languages`,
         crossDomain: true,
         responseType: 'json',
       })
@@ -19,7 +20,6 @@ export function fetchLanguagesAction(pending) {
         return response.data.languages;
       })
       .catch(error => {
-        console.log(error)
         dispatch(actions.fetchLanguagesError(error));
       });
     }
@@ -29,7 +29,7 @@ export function fetchLanguagesAction(pending) {
 
 export function updateLanguageAction(language) {
   return dispatch => {
-    dispatch(actions.updateLanguage(language));
+      dispatch(actions.updateLanguage(language));
   }
 }
 
@@ -40,7 +40,7 @@ export function fetchTextsAction(language, shouldFetch) {
       dispatch(actions.fetchTextsPending());
       axios({
         method: 'get',
-        url: 'http://45.55.219.221:5000/texts',
+        url: `${REST_API}/texts`,
         crossDomain: true,
         responseType: 'json',
         params: {
@@ -95,18 +95,16 @@ export function fetchStoplistAction(feature, stopwords, stoplistBasis, pending) 
 
       axios({
         method: 'get',
-        url: 'http://45.55.219.221:5000/stopwords',
+        url: `${REST_API}/stopwords`,
         crossDomain: true,
         responseType: 'json',
         params: params
       })
       .then(response => {
-        console.log("success", response.data.stopwords);
         dispatch(actions.fetchStoplistSuccess(response.data.stopwords))
         return response.data.stopwords
       })
       .catch(error => {
-        console.log("error", error.response.data);
         dispatch(actions.fetchStoplistError(error))
       });
     }
@@ -115,13 +113,12 @@ export function fetchStoplistAction(feature, stopwords, stoplistBasis, pending) 
 
 
 export function initiateSearchAction(source, target, params, stopwords, pending) {
-  console.log('initiating search', params);
   return dispatch => {
     if (!pending) {
       dispatch(actions.initiateSearchPending());
       axios({
           method: 'post',
-          url: 'http://45.55.219.221:5000/parallels/',
+          url: `${REST_API}/parallels/`,
           crossDomain: true,
           headers: {
             contentType: 'x-www-form-urlencoded'
@@ -147,24 +144,19 @@ export function initiateSearchAction(source, target, params, stopwords, pending)
           }
       })
       .then(response => {
-        console.log(response)
         if (response.headers.location !== undefined) {
           const searchID = response.headers.location.split('/')[4];
-          console.log('got id', searchID);
           dispatch(actions.initiateSearchSuccess(searchID));
           return searchID;
         }
 
         if (response.data.parallels !== undefined) {
-          console.log('initiate got results', response.data.parallels)
-
           dispatch(actions.fetchResultsSuccess(response.data.parallels));
         }
         
         return undefined;
       })
       .catch(error => {
-        console.log(error);
         dispatch(actions.initiateSearchError(error));
       });
     }
@@ -173,18 +165,16 @@ export function initiateSearchAction(source, target, params, stopwords, pending)
 
 
 export function getSearchStatusAction(searchID, pending) {
-  console.log('Getting search status');
   return dispatch => {
     if (!pending) {
       dispatch(actions.getSearchStatusPending);
       axios({
         method: 'get',
-        url: `http://45.55.219.221:5000/parallels/${searchID}/status/`,
+        url: `${REST_API}/parallels/${searchID}/status/`,
         crossDomain: true,
         responseType: 'json'
       })
       .then(response => {
-        console.log('got status', response.data.status);
         const done = response.data.status === 'Done';
         dispatch(actions.getSearchStatusSuccess(response.data.status, done));
       })
@@ -197,18 +187,16 @@ export function getSearchStatusAction(searchID, pending) {
 
 
 export function fetchResultsAction(searchID, pending) {
-  console.log('Fetching results')
   return dispatch => {
     if (!pending) {
       dispatch(actions.fetchResultsPending());
       axios({
           method: 'get',
-          url: `http://45.55.219.221:5000/parallels/${searchID}`,
+          url: `${REST_API}/parallels/${searchID}`,
           crossDomain: true,
           responseType: 'json'
       })
       .then(response => {
-        console.log('Got results', response.data.parallels.length)
         dispatch(actions.fetchResultsSuccess(response.data.parallels));
         return response.data.parallels;
       })
