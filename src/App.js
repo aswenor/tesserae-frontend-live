9c01d38eee1f797b72e8ed5af94240120809ca45
Jeff Kinnison <jeffdkinnison@gmail.com>
2020-04-07 11:34:30 -0400

    remove extraneous imports

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Box from '@material-ui/core/Box';
 
 import NavBar from './components/common/NavBar';
 import Search from './components/search/';
-import theme from './theme';
 
 class App extends Component {
   render() {
