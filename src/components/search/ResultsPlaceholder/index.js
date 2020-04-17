97ab11da2537f0dcd111b17a4ef7d4e5b01eaff6
Jeff Kinnison <jeffdkinnison@gmail.com>
2020-04-17 16:08:41 -0400

    sizing, placement, and appearance of icons

--- src/components/search/ResultsPlaceholder/index.js
+++ src/components/search/ResultsPlaceholder/index.js
@@ -4,13 +4,8 @@ import { connect } from 'react-redux';
 
 import { makeStyles } from '@material-ui/styles';
 import Box from '@material-ui/core/Box';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Hidden from '@material-ui/core/Hidden';
 import Typography from '@material-ui/core/Typography';
 
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
@@ -19,105 +14,110 @@ import { getSearchStatusAction, fetchResultsAction } from '../../../api/corpus';
 
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-        height: '100vh',
-        flexDirection: "column",
-        justifyContent: "center",
-        paddingTop: '30%'
-    },
-    spacer: {
-        height: '30vh'
-    },
-    content: {
-    },
-    card: {
-        height: '30vh'
-    },
-    icon: {
-        height: '20vh',
-        width: '20vh'
-    }
+  root: {
+    display: 'flex',
+    height: '100vh',
+    flexDirection: "column",
+    justifyContent: "center",
+    paddingTop: '30%'
+  },
+  spacer: {
+    display: 'block',
+    marginTop: '25vh',
+    marginLeft: '40%'
+  },
+  icon: {
+    height: '20vh',
+    marginLeft: '25px',
+    width: '20vh'
+  },
+  text: {
+    // color: "#aeaeae",
+    display: 'inline-block',
+    fontSize: 18,
+    marginTop: '20px'
+  }
 }));
 
 
-function AwaitingSearchCard(props) {
-    const { classes } = props;
-    return (
-        <Card className={classes.card}>
-            <CardMedia>
-                <ArrowBackIcon />
-            </CardMedia>
-            <CardContent>
-                <Typography variant="subtitle1">
-                    Run a Search to find parallels.
-                </Typography>
-            </CardContent>
-        </Card>
-    );
-}
-
-
-function SearchInProgressCard(props) {
-    const { classes } = props;
-    return (
-        <Card className={classes.card}>
-            <CardMedia>
-                <CircularProgress />
-            </CardMedia>
-            <CardContent>
-                <Typography variant="subtitle1">
-                    Finding parallels.
-                </Typography>
-            </CardContent>
-        </Card>
-    );
-}
-
-
 function ResultsPlaceholder(props) {
-    const { asyncPending, fetchResults, getSearchStatus, results, searchID,
-            searchInProgress, shouldInitiateSearch, status } = props;
-    const classes = useStyles(props);
+  const { asyncPending, fetchResults, getSearchStatus, results, searchID,
+          searchInProgress, shouldInitiateSearch, status } = props;
+  const classes = useStyles(props);
 
-    if (results.length === 0) {
-        if (searchInProgress && (status === null || status.toLowerCase() !== 'done')) {
-            setTimeout(() => getSearchStatus(searchID, asyncPending), 500);
-        }
-        
-        if (!searchInProgress && status !== null && status.toLowerCase() === 'done') {
-            fetchResults(searchID, asyncPending);
-        }
+  if (results.length === 0) {
+    if (searchInProgress && (status === null || status.toLowerCase() !== 'done')) {
+      setTimeout(() => getSearchStatus(searchID, asyncPending), 500);
     }
+    
+    if (!searchInProgress && status !== null && status.toLowerCase() === 'done') {
+      fetchResults(searchID, asyncPending);
+    }
+  }
 
-    const card = !searchInProgress ? <AwaitingSearchCard classes={classes} /> : <SearchInProgressCard classes={classes} />;
-
-    return (
-        <Box
-            alignItems="center"
-            className={classes.content}
-            justifyContent="center"
-            width="100%"
-        >
-            {card}
-        </Box>
-    );
+  return (
+    <Hidden only={['xs', 'sm']}>
+      <Box
+        display="flex"
+        flexGrow={1}
+        flexDirection="row"
+      >
+        {!searchInProgress
+         ?  <Box
+              className={classes.spacer}
+            >
+              <ArrowBackIcon
+                className={classes.icon}
+                color="primary"
+                style={{ fontSize: 9000 }}
+                viewBox="4 4 18 18"
+              />
+              <br />
+              <Typography
+                className={classes.text}
+                color="primary"
+                variant="subtitle1">
+                Run a search to find parallels.
+              </Typography>
+            </Box>
+         :  <Box
+              className={classes.spacer}
+            >
+              <CircularProgress
+                className={classes.icon}
+                thickness={2.5}
+                size={'20vh'}
+              />
+              <br />
+              <Typography
+                align="left"
+                className={classes.text}
+                color="primary"
+                variant="subtitle1"
+              >
+                Searching. This may take a moment.
+              </Typography>
+            </Box>
+        }
+      </Box>
+    </Hidden>
+  );
 }
 
 
 const mapStateToProps = state => ({
-    asyncPending: state.asyncPending,
-    results: state.results,
-    searchID: state.searchID,
-    searchInProgress: state.searchInProgress,
-    shouldInitiateSearch: state.shouldInitiateSearch,
-    status: state.status
+  asyncPending: state.asyncPending,
+  results: state.results,
+  searchID: state.searchID,
+  searchInProgress: state.searchInProgress,
+  shouldInitiateSearch: state.shouldInitiateSearch,
+  status: state.status
 });
 
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-    fetchResults: fetchResultsAction,
-    getSearchStatus: getSearchStatusAction,
+  fetchResults: fetchResultsAction,
+  getSearchStatus: getSearchStatusAction,
 }, dispatch)
 
 
