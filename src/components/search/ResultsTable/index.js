740cec914cd22f2766495e3e42c98b56006c6c73
Jeff Kinnison <jeffdkinnison@gmail.com>
2020-05-07 12:43:43 -0400

    document the ResultsTable

--- src/components/search/ResultsTable/index.js
+++ src/components/search/ResultsTable/index.js
@@ -21,21 +21,21 @@ import uniq from 'lodash/uniqBy';
 
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
 import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
 import { Typography } from '@material-ui/core';
 
 import { fetchResultsAction,
          getSearchStatusAction,
          updateCurrentPageAction,
          updateRowsPerPageAction, 
-         initiateSearchAction} from '../../../api/corpus';
+         initiateSearchAction } from '../../../api/corpus';
 import ResultsPlaceholder from '../ResultsPlaceholder';
+import ResultsTableBody from '../ResultsTableBody';
+import ResultsTableHeader from '../ResultsTableHeader';
+import { isPropertyAccessExpression } from 'typescript';
 
+
+/** CSS styles to apply to the component. */
 const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
@@ -47,249 +47,37 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
-const cellStyles = makeStyles(theme => ({
-  numberCell: {
-    width: '10%'
-  },
-  snippetCell: {
-    width: '20%'
-  },
-  matchesCell: {
-    width: '10%'
-  },
-}));
-
-
-function ResultsTableHeader(props) {
-  const { sortHeader, sortOrder, updateSortHeader, updateSortOrder } = props;
-  const sortDirection = sortOrder === 1 ? 'asc' : 'desc';
-
-  const classes = cellStyles();
-
-  const handleSortUpdate = header => {
-    const newSortHeader = header.toLowerCase();
-    const newSortOrder = sortHeader === newSortHeader ? -sortOrder : -1;
-    updateSortHeader(newSortHeader);
-    updateSortOrder(newSortOrder);
-  }
-
-  return (
-    <TableHead>
-      <TableRow>
-        <TableCell
-          align="center"
-          className={classes.numberCell}
-          key="number"
-          variant="head"
-        >
-        </TableCell>
-        <TableCell
-          align="center"
-          className={classes.snippetCell}
-          key="source"
-          onClick={() => handleSortUpdate('source_tag')}
-          sortDirection={sortHeader === 'source_tag' ? sortDirection : false}
-          variant="head"
-        >
-          <TableSortLabel
-            active={sortHeader === 'source_tag'}
-            direction={sortDirection}
-          >
-            <Typography variant="h6"><b>Source</b></Typography>
-          </TableSortLabel>
-        </TableCell>
-        <TableCell
-          align="center"
-          className={classes.snippetCell}
-          key="target"
-          onClick={() => handleSortUpdate('target_tag')}
-          sortDirection={sortHeader === 'target_tag' ? sortDirection : false}
-          variant="head"
-        >
-          <TableSortLabel
-            active={sortHeader === 'target_tag'}
-            direction={sortDirection}
-          >
-            <Typography variant="h6"><b>Target</b></Typography>
-          </TableSortLabel>
-        </TableCell>
-        <TableCell
-          align="center"
-          className={classes.matchesCell}
-          key="matches"
-          variant="head"
-        >
-          <Typography variant="h6"><b>Match Features</b></Typography>
-        </TableCell>
-        <TableCell
-          align="center"
-          className={classes.numberCell}
-          key="score"
-          onClick={() => handleSortUpdate('score')}
-          sortDirection={sortHeader === 'score' ? sortDirection : false}
-          variant="head"
-        >
-          <TableSortLabel
-            active={sortHeader === 'score'}
-            direction={sortDirection}
-          >
-            <Typography variant="h6"><b>Score</b></Typography>
-          </TableSortLabel>
-        </TableCell>
-      </TableRow>
-    </TableHead>
-  );
-}
-
-
-function ResultsTableBody(props) {
-  const { results } = props;
-  const classes = cellStyles();
-
-  const bodyCells = results.map((item, idx) => {
-    let source_snippet = [];
-    let target_snippet = [];
-
-    let source_snippet_tokens = item.source_snippet.split(' ');
-    let target_snippet_tokens = item.target_snippet.split(' ');
-
-    let source_indices = uniq(item.highlight.map(x => x[0])).sort();
-    let target_indices = uniq(item.highlight.map(x => x[1])).sort();
-
-    console.log(`${item.highlight}; ${source_indices}; ${target_indices}`);
-
-    let start = 0;
-    let end = null;
-    let slice = null;
-    let component = null;
-
-    while (source_indices.length > 0) {
-      end = source_indices.shift();
-      if (end === 0) {
-        end = 1;
-      }
-      slice = source_snippet_tokens.slice(start, end);
-      // console.log(start, end, slice);
-      source_snippet.push(
-        <Typography
-          color={slice.length > 1 ? 'textPrimary' : 'primary'}
-          component="span"
-          key={`${item.source_tag} ${start},${end}`}
-        >
-          {` ${slice.join(' ')}`}
-        </Typography>
-      );
-      start = end;
-    }
-
-    start = 0;
-    end = null;
-    slice = null;
-    component = null;
-
-    while (target_indices.length > 0) {
-      end = target_indices.shift();
-      if (end === 0) {
-        end = 1;
-      }
-      slice = target_snippet_tokens.slice(start, end);
-      target_snippet.push(
-        <Typography
-          color={slice.length > 1 ? 'textPrimary' : 'primary'}
-          component="span"
-          key={`${item.target_tag} ${start},${end}`}
-        >
-          {` ${slice.join(' ')}`}
-        </Typography>
-      );
-      start = end;
-    }
-
-    return (
-      <TableRow
-        hover
-        tabIndex={-1}
-        key={item.source_tag + item.target_tag + item.matched_features.join(', ')}
-      >
-        <TableCell
-          className={classes.numberCell}
-          variant="body"
-        >
-          <Typography
-            align="left"
-          >
-            {idx + 1}
-          </Typography>
-        </TableCell>
-        <TableCell
-          align="left"
-          className={classes.snippetCell}
-          variant="body"
-        >
-          <Typography><b>{item.source_tag}</b>:</Typography>
-            {source_snippet}
-        </TableCell>
-        <TableCell
-          align="left"
-          className={classes.snippetCell}
-          size="small"
-          style={{maxWidth: '10px'}}
-          variant="body"
-        >
-          <Typography><b>{item.target_tag}</b>:</Typography>
-            {target_snippet}
-        </TableCell>
-        <TableCell
-          align="center"
-          className={classes.matchesCell}
-          size="small"
-          style={{maxWidth: '1px'}}
-          variant="body"
-        >
-          <Typography>
-            {item.matched_features.join(', ')}
-            </Typography>
-        </TableCell>
-        <TableCell
-          align="center"
-          className={classes.numberCell}
-          variant="body"
-        >
-          <Typography>
-            <b>{item.score > 10 ? Math.round(item.score) : 10}</b>
-          </Typography>
-        </TableCell>
-      </TableRow>
-    );
-  });
-
-  return (
-    <TableBody>
-      {bodyCells}
-    </TableBody>
-  );
-}
-
-
+/**
+ * Formatted table to display search results.
+ * 
+ * @component
+ */
 function ResultsTable(props) {
-  const { asyncPending, currentPage, fetchResults, getSearchStatus, initiateSearch, resultCount, results,
-          rowsPerPage, searchID, searchParams, shouldFetchResults, shouldInitiateSearch,
-          sourceText, status, stopwords, targetText, updateCurrentPage, updateRowsPerPage } = props;
+  const { asyncPending, currentPage, initiateSearch, results,
+          rowsPerPage, searchParams, shouldInitiateSearch,
+          sourceText, stopwords, targetText, updateCurrentPage,
+          updateRowsPerPage } = props;
   
+  /** Managers for UI sorting state. */
   const [ sortHeader, setSortHeader ] = useState('score');
   const [ sortOrder, setSortOrder ] = useState(1);
 
+  /** CSS styles and global theme. */
   const classes = useStyles(props);
 
+  /** Labels for the columns. */
   const headerLabels = ['', 'Source', 'Target', 'Matched On', 'Score'];
 
+  // Kick off a search if all parameters are selected.
   if (shouldInitiateSearch) {
     initiateSearch(sourceText, targetText, searchParams, stopwords, asyncPending);
   }
 
+  /** Get the indices of results to display in the table. */
   const start = currentPage * rowsPerPage;
   const end = Math.min(start + rowsPerPage, results.length);
 
+  /** Get the results entries by sorting and slicing. */
   const displayResults = results.sort((a, b) => {
     if (b[sortHeader] < a[sortHeader]) {
       return -1 * sortOrder;
@@ -302,6 +90,9 @@ function ResultsTable(props) {
     }
   }).slice(start, end);
 
+  // If a search has not been run and no results are available, display a
+  // placeholder that points to the parameters form or shows a spinning
+  // load bar. Otherwise, show the results.
   return (
     <div className={classes.root}>
       { results.length === 0
@@ -336,6 +127,107 @@ function ResultsTable(props) {
 }
 
 
+ResultsTable.propTypes = {
+  /**
+   * True if an AJAX request is in progress.
+   */
+  asyncPending: PropTypes.bool,
+
+  /**
+   * The current page of results to display.
+   */
+  currentPage: PropTypes.number,
+
+  /**
+   * Callback to kick off a search.
+   */
+  initiateSearch: PropTypes.func,
+
+  /**
+   * Array of search results retrieved frmo the REST API.
+   */
+  results: PropTypes.arrayOf(
+    PropTypes.shape({
+      /**
+       * Pairs of source/target snippet token indices corresponding to matches.
+       */
+      highlight: PropTypes.arrayOf(propTypes.arrayOf(number)),
+
+      /**
+       * Score of the match.
+       */
+      score: PropTypes.number,
+
+      /**
+       * Matching passage of source text.
+       */
+      source_snippet: PropTypes.string,
+
+      /**
+       * Locus of matching passage of source text.
+       */
+      source_tag: PropTypes.string,
+
+      /**
+       * Matching passage of target text.
+       */
+      target_snippet: PropTypes.string,
+
+      /**
+       * Locus of matching passage of source text.
+       */
+      target_tag: PropTypes.string,
+    })
+  ),
+
+  /**
+   * The number of results to show per table page.
+   */
+  rowsPerPage: PropTypes.number,
+
+  /**
+   * Selected parameters for the search.
+   */
+  searchParams: PropTypes.object,
+
+  /**
+   * True when parameters are selected and the user kicks off a search.
+   */
+  shouldInitiateSearch: PropTypes.bool,
+
+  /**
+   * The source text to use in the search.
+   */
+  sourceText: PropTypes.object,
+
+  /**
+   * List of words to ignore in the search.
+   */
+  stopwords: PropTypes.array,
+
+  /**
+   * The target text to use in the search.
+   */
+  targetText: PropTypes.object,
+
+  /**
+   * Callback to change the currently displayed page.
+   */
+  updateCurrentPage: PropTypes.func,
+
+  /**
+   * Callback to change the number of results displayed per page.
+   */
+  updateRowsPerPage: PropTypes.func
+}
+
+
+/**
+ * Add redux store state to this component's props.
+ * 
+ * @param {object} state The global state of the application.
+ * @returns {object} Members of the global state to provide as props.
+ */
 const mapStateToProps = state => ({
   asyncPending: state.asyncPending,
   currentPage: state.currentPage,
@@ -354,6 +246,10 @@ const mapStateToProps = state => ({
 });
 
 
+/**
+ * Add redux store actions to this component's props.
+ * @param {funciton} dispatch The redux dispatch function.
+ */
 const mapDispatchToProps = dispatch => bindActionCreators({
   initiateSearch: initiateSearchAction,
   fetchResults: fetchResultsAction,
@@ -363,4 +259,5 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 }, dispatch);
 
 
+// Do redux binding here.
 export default connect(mapStateToProps, mapDispatchToProps)(ResultsTable);
