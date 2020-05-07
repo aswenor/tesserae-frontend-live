f1a2e6fd8fb5265394140fc75030cabe1f326095
Jeff Kinnison <jeffdkinnison@gmail.com>
2020-05-07 12:43:11 -0400

    split ResultsTable into multiple files for brevity

---
+++ src/components/search/ResultsTableBody/index.js
@@ -0,0 +1,259 @@
+/**
+ * @fileoverview Styled header for the Tesserae search results table.
+ * 
+ * @author [Jeff Kinnison](https://github.com/jeffkinnison)
+ * 
+ * @exports ResultsTableBody
+ * 
+ * @requires NPM:react
+ * @requires NPM:prop-types
+ * @requires NPM:@material-ui/core
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
+
+
+/** CSS styles to apply to table cells. */
+const cellStyles = makeStyles(theme => ({
+  numberCell: {
+    width: '10%'
+  },
+  snippetCell: {
+    width: '20%'
+  },
+  matchesCell: {
+    width: '10%'
+  },
+}));
+
+
+/**
+ * Highlight relevant tokens in a result snippet.
+ * 
+ * @param {String} snippet The snippet to highlight.
+ * @param {Array} matchIndices The indices of the tokens to highlight.
+ * @returns {Array} A list of Typography components with matches highlighted.
+ */
+function highlightMatches(snippet, tag, matchIndices) {
+  /** Array of highlighted tokens. */
+  let highlightedSnippet = [];
+
+  /** Split the snippet along whitespace. */
+  let snippetTokens = snippet.split(' ');
+  
+  /** The current token index to inspect. */
+  let current = 0;
+
+  /** The next match token index. */
+  let next = null;
+
+  /** The current slice of the snippet to wrap. */
+  let slice = null;
+
+  // Iterate over the match indices, extract token slices, and highlight
+  // relevant tokens.
+  while (matchIndices.length > 0 && current < snippetTokens.length) {
+    // Get the index of the next match token.
+    next = matchIndices.shift();
+
+    // If the current token is the next match token, highlight it.
+    if (current === next) {
+      slice = snippetTokens.slice(current, next + 1);
+      highlightedSnippet.push(
+        <Typography
+            color="primary"
+            component="span"
+            key={`${tag} ${current},${next + 1}`}
+          >
+            {` ${slice.join(' ')}`}
+          </Typography>
+      );
+      current = next + 1;
+    }
+
+    // If no more matches are found, slice into the remaining tokens and
+    // wrap them without a highlight.
+    else if (next === undefined) {
+      slice = source_snippet_tokens.slice(current);
+
+      highlightedSnippet.push(
+        <Typography
+          color="textPrimary"
+          component="span"
+          key={`${tag} ${current},${snippetTokens.length}`}
+        >
+          {` ${slice.join(' ')}`}
+        </Typography>
+      );
+      
+      current = snippetTokens.length;
+    }
+
+    // Otherwise, get the span of tokens leading up to the next match token
+    // and wrap them without highlight, then get the match token and
+    // highlight it.
+    else {
+      slice = snippetTokens.slice(current, next);
+      source_snippet.push(
+        <Typography
+          color="textPrimary"
+          component="span"
+          key={`${tag} ${current},${next}`}
+        >
+          {` ${slice.join(' ')}`}
+        </Typography>
+      );
+
+      slice = source_snippet_tokens.slice(next, next + 1);
+      source_snippet.push(
+        <Typography
+          color="primary"
+          component="span"
+          key={`${item.source_tag} ${next},${next + 1}`}
+        >
+          {` ${slice.join(' ')}`}
+        </Typography>
+      );
+
+      current = next + 1;
+    }
+  }
+
+  return highlightedSnippet;
+}
+
+
+/**
+ * Sequence of cells with search results.
+ * 
+ * @component
+ */
+function ResultsTableBody(props) {
+  const { results } = props;
+
+  /** CSS styles and global theme. */
+  const classes = cellStyles();
+
+  /** Attempt to highlight the match tokens in each result snippet. */
+  const bodyCells = results.map((item, idx) => {
+    
+
+    // Get the indices of match words in each snippet.
+    let sourceIndices = uniq(item.highlight.map(x => x[0])).sort();
+    let targetIndices = uniq(item.highlight.map(x => x[1])).sort();
+
+    const sourceSnippet = highlightMatches(item.source_snippet, item.source_tag, sourceIndices);
+    const targetSnippet = highlightMatches(item.target_snippet, item.target_tag, targetIndices);
+
+    return (
+      <TableRow
+        hover
+        tabIndex={-1}
+        key={item.source_tag + item.target_tag + item.matched_features.join(', ')}
+      >
+        <TableCell
+          className={classes.numberCell}
+          variant="body"
+        >
+          <Typography
+            align="left"
+          >
+            {idx + 1}
+          </Typography>
+        </TableCell>
+        <TableCell
+          align="left"
+          className={classes.snippetCell}
+          variant="body"
+        >
+          <Typography><b>{item.source_tag}</b>:</Typography>
+            {sourceSnippet}
+        </TableCell>
+        <TableCell
+          align="left"
+          className={classes.snippetCell}
+          size="small"
+          style={{maxWidth: '10px'}}
+          variant="body"
+        >
+          <Typography><b>{item.target_tag}</b>:</Typography>
+            {targetSnippet}
+        </TableCell>
+        <TableCell
+          align="center"
+          className={classes.matchesCell}
+          size="small"
+          style={{maxWidth: '1px'}}
+          variant="body"
+        >
+          <Typography>
+            {item.matched_features.join(', ')}
+            </Typography>
+        </TableCell>
+        <TableCell
+          align="center"
+          className={classes.numberCell}
+          variant="body"
+        >
+          <Typography>
+            <b>{item.score > 10 ? Math.round(item.score) : 10}</b>
+          </Typography>
+        </TableCell>
+      </TableRow>
+    );
+  });
+
+  return (
+    <TableBody>
+      {bodyCells}
+    </TableBody>
+  );
+}
+
+
+ResultsTableBody.propTypes = {
+  /**
+   * List of results as specified in the REST API.
+   */
+  results: PropTypes.arrayOf(
+    PropTypes.shape({
+      /**
+       * Pairs of source/target snippet token indices corresponding to matches.
+       */
+      highlight: PropTypes.arrayOf(propTypes.arrayOf(number)),
+
+      /**
+       * Score of the match.
+       */
+      score: PropTypes.number,
+
+      /**
+       * Matching passage of source text.
+       */
+      source_snippet: PropTypes.string,
+
+      /**
+       * Locus of matching passage of source text.
+       */
+      source_tag: PropTypes.string,
+
+      /**
+       * Matching passage of target text.
+       */
+      target_snippet: PropTypes.string,
+
+      /**
+       * Locus of matching passage of source text.
+       */
+      target_tag: PropTypes.string,
+    })
+  )
+};
+
+
+export default ResultsTableBody;
---
+++ src/components/search/ResultsTableHeader/index.js
@@ -0,0 +1,160 @@
+/**
+ * @fileoverview Styled header for the Tesserae search results table.
+ * 
+ * @author [Jeff Kinnison](https://github.com/jeffkinnison)
+ * 
+ * @exports ResultsTableHeader
+ * 
+ * @requires NPM:react
+ * @requires NPM:prop-types
+ * @requires NPM:@material-ui/core
+ */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Typography from '@material-ui/core/Typography';
+
+
+/** CSS styles to apply to table cells. */
+const cellStyles = makeStyles(theme => ({
+  numberCell: {
+    width: '10%'
+  },
+  snippetCell: {
+    width: '20%'
+  },
+  matchesCell: {
+    width: '10%'
+  },
+}));
+
+
+/**
+ * Header cells for the table with sorting buttons.
+ * 
+ * @component
+ */
+function ResultsTableHeader(props) {
+  const { sortHeader, sortOrder, updateSortHeader, updateSortOrder } = props;
+
+  /** Map sort direction from number to string. */
+  const sortDirection = sortOrder === 1 ? 'asc' : 'desc';
+
+  /** CSS styles and global theme. */
+  const classes = cellStyles();
+
+  /**
+   * Update the column being sorted or the sorting order.
+   * 
+   * @param {String} header
+   */
+  const handleSortUpdate = header => {
+    // Normalize the new header.
+    const newSortHeader = header.toLowerCase();
+
+    // If the header has not changed, change the order instead.
+    // If a new header was selected, change to descending order.
+    const newSortOrder = sortHeader === newSortHeader ? -sortOrder : -1;
+
+    // Commit the changes.
+    updateSortHeader(newSortHeader);
+    updateSortOrder(newSortOrder);
+  }
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell
+          align="center"
+          className={classes.numberCell}
+          key="number"
+          variant="head"
+        >
+        </TableCell>
+        <TableCell
+          align="center"
+          className={classes.snippetCell}
+          key="source"
+          onClick={() => handleSortUpdate('source_tag')}
+          sortDirection={sortHeader === 'source_tag' ? sortDirection : false}
+          variant="head"
+        >
+          <TableSortLabel
+            active={sortHeader === 'source_tag'}
+            direction={sortDirection}
+          >
+            <Typography variant="h6"><b>Source</b></Typography>
+          </TableSortLabel>
+        </TableCell>
+        <TableCell
+          align="center"
+          className={classes.snippetCell}
+          key="target"
+          onClick={() => handleSortUpdate('target_tag')}
+          sortDirection={sortHeader === 'target_tag' ? sortDirection : false}
+          variant="head"
+        >
+          <TableSortLabel
+            active={sortHeader === 'target_tag'}
+            direction={sortDirection}
+          >
+            <Typography variant="h6"><b>Target</b></Typography>
+          </TableSortLabel>
+        </TableCell>
+        <TableCell
+          align="center"
+          className={classes.matchesCell}
+          key="matches"
+          variant="head"
+        >
+          <Typography variant="h6"><b>Match Features</b></Typography>
+        </TableCell>
+        <TableCell
+          align="center"
+          className={classes.numberCell}
+          key="score"
+          onClick={() => handleSortUpdate('score')}
+          sortDirection={sortHeader === 'score' ? sortDirection : false}
+          variant="head"
+        >
+          <TableSortLabel
+            active={sortHeader === 'score'}
+            direction={sortDirection}
+          >
+            <Typography variant="h6"><b>Score</b></Typography>
+          </TableSortLabel>
+        </TableCell>
+      </TableRow>
+    </TableHead>
+  );
+}
+
+
+ResultsTableHeader.propTypes = {
+  /**
+   * Header on which to search.
+   */
+  sortHeader: PropTypes.oneOf(['source_tag', 'target_tag', 'score']),
+
+  /**
+   * Order that cells are sorted (descending or ascending).
+   */
+  sortOrder: PropTypes.oneOf([-1, 1]),
+
+  /**
+   * Callback to sort by a new header.
+   */
+  updateSortHeader: PropTypes.func,
+
+  /**
+   * Callback to reverse the sort direction.
+   */
+  updateSortOrder: PropTypes.func
+};
+
+
+export default ResultsTableHeader;
