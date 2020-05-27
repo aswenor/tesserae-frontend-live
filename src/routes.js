d384f6363ab038cfc13a03420e0bbff9bcac5e9e
Jeff Kinnison <jeffdkinnison@gmail.com>
2020-05-26 22:45:54 -0400

    put routing information in its own file

---
+++ src/routes.js
@@ -0,0 +1,31 @@
+/**
+ * @fileoverview Top-level routing information for easy page management.
+ * 
+ * @author [Jeff Kinnison](https://github.com/jeffkinnison)
+ * 
+ * @exports App
+ * 
+ * @requires NPM:react
+ * @requires NPM:react-router-dom
+ * @requires ./components/common/NavBar/index.js:NavBar
+ * @requires ./components/search/index.js:Search
+ */
+import Search from './components/search';
+
+
+/**
+ * Routing data for various pages in the app.
+ */
+const routes = [
+  /**
+   * Metadata for a page.
+   * 
+   * @field {string} link The URL path to the page.
+   * @field {string} name The name of the page being linked.
+   * @field {React.Component} component The component to render.
+   */
+  {link: "/", name: "Search", component: Search}
+];
+
+
+export default routes;
