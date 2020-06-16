52668f1948791cb3c7b05b398e4d9fdc505e1ecb
Jeff Kinnison <jeffdkinnison@gmail.com>
2020-06-16 11:39:06 -0500

    remove unused imports

--- src/components/common/PageContainer.js
+++ src/components/common/PageContainer.js
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
 
-import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
@@ -11,18 +7,6 @@ import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import LanguageSelectMenu from './LanguageSelectMenu';
 import NavBar from './NavBar';
 import createTesseraeTheme from '../../theme';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    overflow: 'hidden',
-    width: '100vw'
-  },
-  button: {
-    border: '1px solid #000000',
-    background: '#ffffff'
-  },
-}))
 
 
 function PageContainer(props) {
