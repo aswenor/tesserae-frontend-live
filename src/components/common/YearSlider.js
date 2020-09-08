99e2107856c3fb3c30a6f48f2c8ffdd65af40084
Jeff Kinnison <jeffdkinnison@gmail.com>
2020-09-08 13:32:08 -0500

    common corpus filtering components

---
+++ src/components/common/YearSlider.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { isArray, maxBy, minBy, zip } from 'lodash';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Slider from '@material-ui/core/Slider';
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        marginTop: '40px'
+    }
+}));
+
+
+function YearSlider(props) {
+    const { maxYear, minYear, setYear, year } = props;
+
+    const classes = useStyles();
+
+    const createLabelText = (value) => (
+        value > 0 
+        ? `${Math.abs(value)} C.E.`
+        : `${Math.abs(value)} B.C.E.`
+    );
+
+    const skipZero = (oldVal, newVal) => {
+        if (newVal === 0) {
+            return oldVal < 0 ? 1 : -1;
+        }
+        else {
+            return newVal;
+        }
+    };
+
+    const updateWithZeroSkip = (value) => {
+        if (isArray(value)) {
+            const newYear = zip(year, value).map((x) => skipZero(...x));
+            console.log(newYear);
+            setYear(newYear);
+        }
+        else {
+            setYear(skipZero(value));
+        }
+    }
+
+    return (
+        <Slider
+            aria-labelledby="select date slider"
+            className={classes.root}
+            getAriaValueText={createLabelText}
+            max={maxYear}
+            min={minYear}
+            // onChange={(event, value) => updateWithZeroSkip(value)}
+            onChangeCommitted={(event, value) => updateWithZeroSkip(value)}
+            value={year}
+            valueLabelDisplay="on"
+            valueLabelFormat={createLabelText}
+        />
+    );
+}
+
+
+YearSlider.propTypes = {
+    /**
+     * Maximum allowable year. Automatically detected from corpus.
+     */
+    maxYear: PropTypes.number,
+
+    /**
+     * Minimum allowable year. Automatically detected from corpus.
+     */
+    minYear: PropTypes.number,
+
+    /**
+     * Function to update the selected year.
+     */
+    setYear: PropTypes.func,
+
+    /**
+     * The currently selected year. If an array, displays as a range of years.
+     */
+    year: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.arrayOf(PropTypes.number)
+    ])
+}
+
+
+function mapStateToProps(state) {
+    const maxYear = maxBy(state.corpus.availableTexts, 'year');
+    const minYear = minBy(state.corpus.availableTexts, 'year');
+    return {
+        maxYear: maxYear ? maxYear.year : 1000000,
+        minYear: minYear ? minYear.year : -1000000
+    }
+}
+
+
+export default connect(mapStateToProps)(YearSlider);
