1b65adf36fcc322b46f359dd0f34d155151da0d2
Jeff Kinnison <jeffdkinnison@gmail.com>
2020-10-26 13:43:03 -0400

    remove admin mode and reorder routes

--- src/routes.js
+++ src/routes.js
@@ -10,34 +10,9 @@
  */
 import React from 'react'
 
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
-import PublishIcon from '@material-ui/icons/Publish';
-
-import CorpusDeleter from './components/corpus_manager/delete';
-import CorpusEditor from './components/corpus_manager/edit';
-import CorpusIngester from './components/corpus_manager/ingest';
 import CorpusManager from './components/corpus_manager';
 import Search from './components/search';
 import Multitext from './components/multitext';
-import CorpusViewer from './components/corpus_manager/CorpusViewer';
-
-
-const mode = String(process.env.REACT_APP_MODE);
-
-
-/**
- * Routes to corpus management pages conditioned on admin mode.
- */
-const corpusRoutes = mode.toLowerCase() === 'admin' 
-                     ? [{link: '/corpus', name: 'Corpus', component: CorpusManager}]
-                     : [
-                        {icon: <LibraryBooksIcon />, link: '/corpus', name: 'View the Corpus', component: CorpusManager},
-                        {icon: <PublishIcon />, link: '/corpus/ingest', name: 'Ingest a Text', component: CorpusIngester},
-                        {icon: <EditIcon />, link: '/corpus/edit', name: 'Edit Text Metadata', component: CorpusEditor},
-                        {icon: <DeleteIcon />, link: '/corpus/delete', name: 'Delete a Text', component: CorpusDeleter},
-                       ];
 
 
 /**
@@ -51,8 +26,8 @@ const routes = [
    * @field {string} name The name of the page being linked.
    * @field {React.Component} component The component to render.
    */
+  {link: '/corpus', name: 'Corpus', component: CorpusManager},
   {link: "/", name: "Search", component: Search},
-  ...corpusRoutes,
   {link: "/multitext", name: "Multitext", component: Multitext}
 ];
 
